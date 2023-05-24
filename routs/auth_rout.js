73a481b8a7be35465f78740036b01af23efbcc19
AbdAlftahSalem <abd321alftah@gmail.com>
2023-05-24 16:50:11 +0300

    Add get me requist

--- routs/auth_rout.js
+++ routs/auth_rout.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const validator = require("../utils/validators/user_controller_validator")
-
+const AuthController = require("../controllers/auth_controllers")
 
 const {
-    loginUser, registerUser
+    loginUser, registerUser, getMe
 } = require("../controllers/auth_controllers")
 
 
@@ -12,4 +12,5 @@ const router = express.Router();
 
 router.route("/register").post(validator.registerUser, registerUser)
 router.route("/login").post(validator.loginUser, loginUser)
+router.route("/get-me").get(AuthController.protectRout, getMe)
 module.exports = router;
